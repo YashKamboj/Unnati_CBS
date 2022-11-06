349b05d011a259fdeb77fabb18794e03599a0577
YashKamboj <yashkamboj29@gmail.com>
2022-11-06 09:32:21 +0530

    added circular progress
    Signed-off-by: YashKamboj <yashkamboj29@gmail.com>

--- src/components/Campaign.tsx
+++ src/components/Campaign.tsx
@@ -10,23 +10,53 @@ import {
 import { fromWei, toWei } from "../utils";
 import { useAddRecentTransaction } from "@rainbow-me/rainbowkit";
 import type { ChangeEvent, MouseEvent } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Img from "./imgssss.jpg";
 import Img2 from "./gems.jpeg";
-// import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import CircularProgress, {
+  CircularProgressProps,
+} from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 export type CampaignProps = { projectNumber: number };
 
-
-const percentage = 66;
 // var percentage = () => ( ( fromWei(raisedAmount) / fromWei(goalAmount)) * 100) ;
+
+
+function CircularProgressWithLabel(
+  props: CircularProgressProps & { value: number },
+) {
+  return (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          variant="caption"
+          component="div"
+          color="text.secondary"
+        >{`${Math.round(props.value)}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
 
 function Campaign({ projectNumber }: CampaignProps) {
   DEBUG && console.log("projectNumber: ", projectNumber);
 
-  const [value, setValue] = useState<string>("");
-
+  const [percentage, setpercentage] = useState(0);
+  
   const publishedProjsAddress = usePublishedProjs(projectNumber);
   const projTitle = useProjTitle(publishedProjsAddress || "");
   const projDescription = useProjDescription(publishedProjsAddress || "");
@@ -37,7 +67,9 @@ function Campaign({ projectNumber }: CampaignProps) {
   // function percentage(partialValue, totalValue) {
   //   return (100 * fromWei(raisedAmount)) / x;
   // } 
-
+    useEffect(() => {
+      setpercentage (100 * Number(fromWei(raisedAmount))/ Number(fromWei(goalAmount)));
+  }, []);
   DEBUG &&
     console.log({
       publishedProjsAddress,
@@ -55,39 +87,6 @@ function Campaign({ projectNumber }: CampaignProps) {
     contractAddress: publishedProjsAddress || "",
     functionName: "makeDonation",
   });
-
-  const handleValue = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-
-    const inputValue = e.target.value;
-    DEBUG && console.log("value: ", inputValue);
-
-    // set value
-    setValue(inputValue);
-  };
-
-  const handleDonate = async (e: MouseEvent<HTMLButtonElement>) => {
-    try {
-      e.preventDefault();
-
-      const valueToWei = toWei(value);
-      DEBUG && console.log("valueToWei: ", valueToWei);
-
-      const tx = await writeAsync({
-        overrides: {
-          value: valueToWei,
-        },
-      });
-      console.log("tx >>> ", tx);
-
-      addRecentTransaction({
-        hash: tx.hash,
-        description: `Donate ${value} MATIC`,
-      });
-    } catch (error) {
-      console.log("errror >>> ", error);
-    }
-  };
 
   if (
     !publishedProjsAddress ||
@@ -107,29 +106,10 @@ function Campaign({ projectNumber }: CampaignProps) {
         <p className="text-gray-700 text-base">{projDescription}</p>
       </div>
       <div style={{width: "4.5rem", position: "absolute"}}>
-  {/* <CircularProgressbar 
-  value={percentage}
-  text={`${percentage}%`}
-  styles={buildStyles({
-    rotation: 0.0,
-    strokeLinecap: 'butt',
-    textSize: '20px',
-    pathTransitionDuration: 0.5,
-    padding: "2rem 10rem",
-    pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
-    textColor: '#f88',
-    trailColor: '#d6d6d6',
-    backgroundColor: '#3e98c7',
-  })}
-/> */}
+<div style={{marginTop: "1.4rem", marginLeft: "1.3rem"}}>
+  <CircularProgressWithLabel value={percentage} />
+</div>
 
-<CircularProgressbarWithChildren value={66}>
-  {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-  <img style={{ width: 40, marginTop: -5 }} src="https://i.imgur.com/b9NyUGm.png" alt="doge" />
-  <div style={{ fontSize: 12, marginTop: -5 }}>
-    <strong>66%</strong>
-  </div>
-</CircularProgressbarWithChildren>
 </div>
       <div className="px-20  pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 inline-block.">
@@ -144,15 +124,7 @@ function Campaign({ projectNumber }: CampaignProps) {
 
 
         <div className="flex justify-around items-center py-2">
-          {/* <input
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-            type="number"
-            placeholder="0.000"
-            min={0}
-            step="0.001"
-            required
-            onChange={handleValue}
-          /> */}
+
           <a href= {`/Campaign?id=${projectNumber}`} >
           <button
             className="flex-shrink-0  text-lg text-white py-1 px-12 rounded-3xl"
